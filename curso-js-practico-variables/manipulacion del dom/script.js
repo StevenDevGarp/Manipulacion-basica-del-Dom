c218cbd46b4ce9c8c3635c45fba96bc6dfad42ae
Steven Garcia <Stevgtpayes@gmail.com>
2022-12-04 15:55:49 -0600

    agregando imagenes a el html con las propiedades creatElement, innerHtml, append, appendChild, setAttribute

--- curso-js-practico-variables/manipulacion del dom/script.js
+++ curso-js-practico-variables/manipulacion del dom/script.js
@@ -1,3 +1,15 @@
-const p = document.querySelector("p");
-const pclass = document.querySelector(".classP");
-const pId = document.querySelector("#pId");
+const h1 = document.querySelector("h1");
+const p = document.querySelector('p');
+const pclass = document.querySelector('.classP');
+const pId = document.querySelector('#pId');
+const input = document.querySelector('input');
+
+
+h1.innerHTML = 'Patito'
+
+input.value = "456";
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg');
+pId.innerHTML = "";
+pId.append(img);
