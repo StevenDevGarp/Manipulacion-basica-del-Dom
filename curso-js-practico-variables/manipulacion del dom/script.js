dfc7ad3e572fd9b339851a8f422afb4759581f73
Steven Garcia <Stevgtpayes@gmail.com>
2022-12-04 18:38:06 -0600

    usando formularios y submit en el addEventListener

--- curso-js-practico-variables/manipulacion del dom/script.js
+++ curso-js-practico-variables/manipulacion del dom/script.js
@@ -1,11 +1,13 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form')
 const calculo1 = document.querySelector('#calculo1');
 const calculo2 = document.querySelector('#calculo2');
 const btncalcular = document.querySelector('#btnCalcular');
 const presult  = document.querySelector('#result');
 
-btncalcular.addEventListener('click', btnOnClick);
+form.addEventListener('submit', submitInputValues);
 
-function btnOnClick(){
+function submitInputValues(event){
+event.preventDefault();
 presult.innerHTML ="resultado es: " + (parseInt(calculo1.value) + parseInt(calculo2.value));
 }
